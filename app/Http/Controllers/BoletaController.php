57a6b35f5aa63031eb6143078307fb8486518d49
george <george.roa@hotmail.com>
2023-05-23 19:41:59 -0500

    Cambios 23052023

--- app/Http/Controllers/BoletaController.php
+++ app/Http/Controllers/BoletaController.php
@@ -6,9 +6,11 @@ use App\Http\Resources\BoletaCollection;
 use App\Models\Boleta;
 use App\Models\BoletaPedido;
 use App\Models\Pedido;
+use PDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+
 
 class BoletaController extends Controller
 {
@@ -42,6 +44,8 @@ class BoletaController extends Controller
         $boleta->total = $request->totalBoleta;
         $boleta->dni = $request->dni;
         $boleta->pago_id = $request->pago;
+        $boleta->igv = $request->igvBoleta;
+        $boleta->subtotal = $request->subTotalBoleta;
         $boleta->save();
 
         $id = $boleta->id;
@@ -90,4 +94,45 @@ class BoletaController extends Controller
     {
         //
     }
+
+    public function listar(){
+
+        $tickets = Boleta::with('user')->where('estado',1)->orderBy('created_at','asc')->get();
+
+        //$ticket = Boleta::where('estado',1)->orderBy('nombre','asc')->get();
+
+        return view('boleta',[
+            'tickets'=>$tickets
+        ]);
+    }
+
+    public function detalle($id){
+
+        $ticket = DB::select('SELECT bp.id, bp.boleta_id, bp.pedido_id, pd.producto_id, pr.nombre, pr.precio ,pd.cantidad, p.total as subtotal, b.total as total, b.created_at, b.igv, b.subtotal as gravada, b.dni FROM boleta_pedidos bp
+                               inner join boletas b on b.id = bp.boleta_id
+                               inner join pedidos p on p.id = bp.pedido_id
+                               inner join pedido_detalles pd on pd.pedido_id=bp.pedido_id
+                               inner join productos pr on pr.id = pd.producto_id
+                               where bp.boleta_id='.$id);
+
+        return view('detalle',['ticket'=>$ticket]);
+
+    }
+
+    public function PDF($id){
+
+        $ticket = DB::select('SELECT bp.id, bp.boleta_id, bp.pedido_id, pd.producto_id, pr.nombre, pr.precio ,pd.cantidad, p.total as subtotal, b.total as total, b.created_at, b.igv, b.subtotal as gravada, b.dni FROM boleta_pedidos bp
+                              inner join boletas b on b.id = bp.boleta_id
+                              inner join pedidos p on p.id = bp.pedido_id
+                              inner join pedido_detalles pd on pd.pedido_id=bp.pedido_id
+                              inner join productos pr on pr.id = pd.producto_id
+                              where bp.boleta_id='.$id);
+
+        $pdf = PDF::loadView('pdf',['ticket'=>$ticket])->setPaper('A6');
+        //$pdf->loadHTML('<h1>Test</h1>');
+        return $pdf->stream();
+
+        return view('pdf',['ticket'=>$ticket]);
+    }
+
 }
---
+++ resources/views/detalle.blade.php
@@ -0,0 +1,107 @@
+
+<div class="col-3" style="width: 25%">
+
+<div class="col-11">
+    <h2>Factura</h2>
+
+    <div class="row my-3">
+        <div class="col-10" style="text-align: center">
+            <h1 >"Cupidos Bar"</h1>
+            <p>RUC: 202111221</p>
+            <p>Av. XXX - XXX Lima-Surco</p>
+        </div>
+    </div>
+
+    <div class="col-2">
+        
+    </div>
+
+</div>
+
+<hr/>
+
+<div class="row fact-info mt-3">
+    <div class="col-3">
+        {{-- @foreach ($ticket as $ticket)
+        <h5>DNI: {{$ticket->dni}}</h5>
+        @endforeach --}}
+        <table>
+    
+            <tr>
+                <th style="text-align: center">ID</th>
+                <th style="text-align: center">Detalle</th>
+                <th style="text-align: center">Cant.</th>
+                <th style="text-align: center">Precio</th>
+                <th style="text-align: center">Total</th>
+            </tr>
+
+            <tbody>
+            @foreach ($ticket as $ticket)
+
+                <tr>
+                    <td>{{$ticket->id}}</td>
+                    <td>{{$ticket->nombre}}</td>
+                    <td style="text-align: right">{{$ticket->cantidad}}</td>
+                    <td style="text-align: right">{{$ticket->precio}}</td>
+                    <td style="text-align: right">{{($ticket->cantidad)*($ticket->precio)}}</td>                 
+                </tr> 
+            
+            @endforeach
+            </tbody>
+        </table>
+
+        <div style="text-align: right">
+            <p>Subtotal: {{$ticket->subtotal}}</p>
+            <p>IGV 18%: {{$ticket->igv}}</p>
+            <p>Total: {{$ticket->total}}</p>
+        </div>
+
+        <hr/>
+
+        <p>DNI: {{$ticket->dni}}</p>
+
+    </div>
+
+</div>
+</div>
+
+
+{{-- <div class="container">
+
+    <div class="row"> 
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+    
+                <h1>Detalle</h1>
+    
+                <table>
+    
+                    <tr>
+                        <th>ID</th>
+                        <th>Detalle</th>
+                        <th>Cant.</th>
+                        <th>Precio</th>
+                        <th>Total</th>
+                    </tr>
+    
+                    @foreach ($ticket as $ticket)
+    
+                        <tr>
+                            <td>{{$ticket->id}}</td>
+                            <td>{{$ticket->nombre}}</td>
+                            <td>{{$ticket->cantidad}}</td>
+                            <td>{{$ticket->precio}}</td>
+                            <td>{{($ticket->cantidad)*($ticket->precio)}}</td>
+                            
+                        </tr> 
+                    
+                    @endforeach
+                </table>
+    
+            <h3>Subtotal:</h1>
+            <h1>TOTAL: {{$ticket->total}}</h1>         
+                
+            </div>        
+        </div>
+    </div>  
+</div> --}}
